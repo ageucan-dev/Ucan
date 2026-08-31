143e06f880d6cc854b2ce4ee04782141b623306a
U CAN AGÊNCIA DE MARKETING <ageucan@gmail.com>
2026-08-31 16:31:50 -0300

    Install GTM-W3SP7J67 on landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,15 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W3SP7J67');`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -44,6 +53,16 @@ export default function RootLayout({
         <script src="/estetica-saude/form-config.js" defer />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3SP7J67"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+
         <header className="site-header site-header--hero" data-original-header>
           <div className="site-header__inner">
             <a
